fc3edadec6b2e352165a0afa3eaeaf31257f1dcc
ahmed <ahmedrsamak@gmail.com>
2018-08-06 22:12:36 +0200

    init files

---
+++ SCI_Driver/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * SCI_Driver.cpp
+ *
+ * Created: 8/6/2018 10:07:13 PM
+ * Author : ahmed
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
